149a1f71e27dd705df1f874d9a882884fcf37b6f
Rishav Ghosh <rishavghosh@Rishavs-MacBook-Air.local>
2024-11-12 06:59:27 +0530

    test update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'profile';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
